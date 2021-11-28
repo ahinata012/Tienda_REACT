1cf0c48d6bf10f783501cb8df2d9da2707d53c03
jimjabid <jabidandresjimenezserrano@gmail.com>
2021-11-28 16:57:17 -0300

    cambios footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -8,6 +8,16 @@ const Footer = () => {
         <Row>
           <Col className="text-center py-3">Copiright &copy; La Tiendita</Col>
         </Row>
+        <Row>
+          <Col className=" text-center">
+            <a className="link__footer">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a className="link__footer">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </Col>
+        </Row>
       </Container>
     </footer>
   );
